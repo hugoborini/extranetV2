f5137a8d181be3be9b39387c556e02d97d49032f
Marie@91380 <hugo.borini@hetic.net>
2021-12-16 17:50:27 +0100

    add start

---
+++ src/Controller/TestController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test", name="test")
+     */
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="homePage")
+     */
+    public function homePage() : Response {
+        return $this->render('test/home.html.twig', [
+            "title" => "wesh from controller",
+        ]);
+    }
+}
